01a0cd31f0814cb6abc82cfc62aa61fd76a9ea28
delirious007 <67654968+delirious007@users.noreply.github.com>
2021-12-08 12:19:59 +0530

    Update to ExportListItemsToPDFFormatter.ts
    Incase some rows in a people columns don't contain any value, line 11 was throwing error. "reading property of undefined".
    fixed it so if the value is undefined it will show a blank.
    If list contains multiline fields pdf was getting printed  throwing value error.
    fixed it so gets value from dom property and if no value prints blank.

--- samples/react-datatable/src/shared/common/ExportListItemsToPDF/ExportListItemsToPDFFormatter.ts
+++ samples/react-datatable/src/shared/common/ExportListItemsToPDF/ExportListItemsToPDFFormatter.ts
@@ -8,7 +8,10 @@ export function pdfCellFormatter(value: any, type: string) {
       value = { text, link, color: 'blue' };
       break;
     case 'SP.FieldUser':
-      value = value.props.displayName;
+      value = value.props ? value.props.displayName : "";
+      break;
+    case 'SP.FieldMultiLineText':
+      value = value.props ? value.props.dangerouslySetInnerHTML.__html : "";
       break;
     default:
       break;
